cf1e414c1fc991850dcac4244b1f8e7e963b25f3
F.K <hankouki4954@gmail.com>
2020-03-18 22:19:26 +0000

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   #callback methods（特定のタイミングで呼び出される）
   before_save { self.email = email.downcase }
+
   #validations
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,5 +12,12 @@ class User < ApplicationRecord
 
   #other Rails' methods
   has_secure_password
+
+  ## 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
   
 end
